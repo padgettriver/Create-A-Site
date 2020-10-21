31faac3b2758391ce51777c133b33d768539e9a6
padgettriver <padgettcriver@gmail.com>
2020-10-21 03:16:09 -0400

    First Time Didn't work added a little diffrent PHP

--- contact.php
+++ contact.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 $name = $Post['name'];
 $email = $Post['email'];
 $message = $Post['message'];
@@ -21,4 +21,15 @@ header("Location: contact.html");
 
 
 
+?> -->
+
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "emailaddress@here.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
 ?>

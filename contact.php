cf415fccbdffc930482584bcbdd256c9062184b5
padgettriver <padgettcriver@gmail.com>
2020-10-21 03:19:06 -0400

    Forgot to add in my email and got rid of old PHP

--- contact.php
+++ contact.php
@@ -1,33 +1,8 @@
-<!-- <?php
-$name = $Post['name'];
-$email = $Post['email'];
-$message = $Post['message'];
-
-$email_form = 'rivercpadgett@gmail.com';
-$email_subject = 'New Contact Sub';
-
-$email_body = "Name: $name .\n". 
-                "Email: $email.\n". 
-                "Message: $message.\n"
-
-$to = "padgettcriver@gmail.com";
-
-$headers = "From: $email \r\n";
-$headers = "Reply to: $email \r\n";
-
-mail($to, $email_subject, $email_body, $headers);
-
-header("Location: contact.html");
-
-
-
-?> -->
-
 <?php $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "emailaddress@here.com";
+$recipient = "padgettcriver@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
